cb56e2eb9f5c682ba65a3828e91068a3a5c1244e
wwaddell <william@wwaddell.com>
2025-09-03 09:09:59 -0500

    feat: added opi5pro installer, profile, dtb

---
+++ installers/orangepi-5-pro/src/main.go
@@ -0,0 +1,98 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/siderolabs/go-copy/copy"
+	"github.com/siderolabs/talos/pkg/machinery/overlay"
+	"github.com/siderolabs/talos/pkg/machinery/overlay/adapter"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	off int64 = 512 * 64
+	dtb       = "rockchip/rk3588s-orangepi-5-pro.dtb"
+)
+
+func main() {
+	adapter.Execute(&opi5Installer{})
+}
+
+type opi5Installer struct{}
+
+type opi5ExtraOptions struct {
+	SPIBoot bool `yaml:"spi_boot,omitempty"`
+}
+
+func (i *opi5Installer) GetOptions(extra opi5ExtraOptions) (overlay.Options, error) {
+	kernelArgs := []string{
+		"console=tty0",
+		"console=ttyS2,115200",
+		"sysctl.kernel.kexec_load_disabled=1",
+		"talos.dashboard.disabled=1",
+	}
+	return overlay.Options{
+		Name:       "orangepi-5-pro",
+		KernelArgs: kernelArgs,
+		PartitionOptions: overlay.PartitionOptions{
+			Offset: 2048 * 10,
+		},
+	}, nil
+}
+
+func (i *opi5Installer) Install(options overlay.InstallOptions[opi5ExtraOptions]) error {
+	if !options.ExtraOptions.SPIBoot {
+		uBootBin := filepath.Join(options.ArtifactsPath, "arm64/u-boot/orangepi-5-pro/u-boot-rockchip.bin")
+
+		if err := uBootLoaderInstall(uBootBin, options.InstallDisk); err != nil {
+			return err
+		}
+	}
+
+	src := filepath.Join(options.ArtifactsPath, "arm64/dtb", dtb)
+	dst := filepath.Join(options.MountPrefix, "boot/EFI/dtb", dtb)
+
+	if err := copyFileAndCreateDir(src, dst); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func copyFileAndCreateDir(src, dst string) error {
+	if err := os.MkdirAll(filepath.Dir(dst), 0o600); err != nil {
+		return err
+	}
+
+	return copy.File(src, dst)
+}
+
+func uBootLoaderInstall(uBootBin, installDisk string) error {
+	f, err := os.OpenFile(installDisk, os.O_RDWR|unix.O_CLOEXEC, 0o666)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", installDisk, err)
+	}
+
+	defer f.Close() //nolint:errcheck
+
+	uboot, err := os.ReadFile(uBootBin)
+	if err != nil {
+		return err
+	}
+
+	if _, err = f.WriteAt(uboot, off); err != nil {
+		return err
+	}
+
+	// NB: In the case that the block device is a loopback device, we sync here
+	// to ensure that the file is written before the loopback device is
+	// unmounted.
+	return f.Sync()
+}
